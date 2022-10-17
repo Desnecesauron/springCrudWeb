3ba5adf7076e0be07e36deeacb3b2167f0a2835a
Desnecesauron <cesinhasampa63@gmail.com>
2022-10-17 14:05:37 -0300

    Updated index

--- frontCrudSpring/src/app/app.component.ts
+++ frontCrudSpring/src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { createMayBeForwardRefExpression } from '@angular/compiler';
 import { Component, createComponent, OnInit } from '@angular/core';
 import { BrowserModule, Title } from '@angular/platform-browser';
 import { __values } from 'tslib';
@@ -26,6 +27,11 @@ export class AppComponent implements OnInit {
   ngOnInit() {
     this.title.setTitle(this.titlePage);
   }
+
+  // interface SignupForm {
+  //   nome: string,
+
+  // }
 
   public dataForm: {
     nome: string;
@@ -74,144 +80,83 @@ export class AppComponent implements OnInit {
   }
 
   public postLogin() {
-    // console.log(this.dataForm)
-
-    if (this.dataForm.cpf == '' && this.dataForm.nome == '') {
+    if (this.dataForm.cpf == '' || this.dataForm.nome.trim() == '') {
       alert('Insira os dados!!');
       return;
+    } else {
+      if (!this.TestaCPF()) {
+        alert('Mó cota fazendo isso pra tu colocar CPF inválido :(');
+        return;
+      } else {
+        var requestOptions = {
+          method: 'GET',
+          redirect: 'follow',
+        };
+
+        fetch(
+          'https://crudclinics.herokuapp.com/listPersons/cpf/' +
+            this.undoFormatAnyCpf(this.dataForm.cpf)
+        )
+          .then((response) => response.text())
+          .then((result) => {
+            var resultJson = JSON.parse(result);
+            console.log('Resultado:' + result);
+            console.log(resultJson.status);
+            if (resultJson.status == '404') {
+              alert('Cadastro não encontrado!');
+              return;
+            }
+
+            if (
+              resultJson.nome == this.dataForm.nome &&
+              resultJson.cpf == this.undoFormatAnyCpf(this.dataForm.cpf)
+            ) {
+              alert('Bem-vindo, ' + this.dataForm.nome + '!');
+            } else {
+              alert('Cadastro não bate!!');
+              return;
+            }
+          })
+          .catch((error) => console.log('error', error));
+      }
     }
-
-    if (!this.nameVerify()) {
-      return;
-    }
-
-    if (!this.TestaCPF()) {
-      alert('Mó cota fazendo isso pra tu colocar CPF inválido :(');
-      return;
-    }
-
-    var requestOptions = {
-      method: 'GET',
-      redirect: 'follow',
-    };
-
-    fetch(
-      'https://crudclinics.herokuapp.com/listPersons/cpf/' +
-        this.desformatAnyCpf(this.dataForm.cpf)
-    )
-      .then((response) => response.text())
-      .then((result) => {
-        var resultJson = JSON.parse(result);
-        console.log('Resultado:' + result);
-        console.log(resultJson.status);
-        if (resultJson.status == '404') {
-          alert('Cadastro não encontrado!');
-          return;
-        }
-
-        if (
-          resultJson.nome == this.dataForm.nome &&
-          resultJson.cpf == this.desformatAnyCpf(this.dataForm.cpf)
-        ) {
-          alert('Bem-vindo, ' + this.dataForm.nome + '!');
-        } else {
-          alert('Cadastro não bate!!');
-          return;
-        }
-      })
-      .catch((error) => console.log('error', error));
 
     return;
   }
 
   public postRegister() {
     console.log(this.dataForm);
+
+    var myHeaders = new Headers();
+    myHeaders.append('Content-Type', 'application/json');
+
+    var raw = JSON.stringify({
+      id: null,
+      nome: this.dataForm.nome,
+      cpf: Number(this.undoFormatAnyCpf(this.dataForm.cpf)),
+      excluido: false,
+      endereco: this.dataForm.endereco,
+      cep: Number(this.undoFormatAnyCpf(this.dataForm.cep)),
+      cidade: this.dataForm.cidade,
+      medico: this.dataForm.medico,
+      crm: this.dataForm.crm,
+    });
+
+    console.log(raw);
+
+    fetch('https://crudclinics.herokuapp.com/listPersons/ins', {
+      method: 'POST',
+      headers: myHeaders,
+      body: raw,
+      redirect: 'follow',
+      mode: 'cors',
+    })
+      .then((response) => response.text())
+      .then((result) => console.log(result))
+      .catch((error) => console.log('error', error));
   }
 
-  public formatCpf() {
-    const input = document.getElementById('CpfText') as HTMLInputElement | null;
-    var value = input?.value;
-
-    if (value == '' || value == null) {
-      return;
-    }
-
-    if (value.trim() == '') {
-      value = value.trim();
-      this.dataForm.cpf = value;
-      return;
-    }
-
-    let valorFormatado = value + '';
-
-    do {
-      if (valorFormatado.includes('.') || valorFormatado.includes('-')) {
-        valorFormatado = valorFormatado.replace('.', '');
-        valorFormatado = valorFormatado.replace('-', '');
-      } else {
-        break;
-      }
-    } while (true);
-
-    if (!this.onlyNumbers(valorFormatado)) {
-      do {
-        if (valorFormatado.length == 0) break;
-        if (!this.onlyNumbers(valorFormatado)) {
-          valorFormatado = valorFormatado.substring(
-            0,
-            valorFormatado.length - 1
-          );
-        } else {
-          break;
-        }
-      } while (true);
-
-      this.dataForm.cpf = valorFormatado;
-      if (valorFormatado.length == 0) return;
-    }
-
-    valorFormatado = valorFormatado
-      .padStart(11, '0') // item 1
-      .substring(0, 11) // item 2
-      .replace(/[^0-9]/, '') // item 3
-      .replace(
-        // item 4
-        /(\d{3})(\d{3})(\d{3})(\d{2})/,
-        '$1.$2.$3-$4'
-      );
-
-    this.dataForm.cpf = valorFormatado;
-    return valorFormatado;
-  }
-
-  public desformatCpf() {
-    const input = document.getElementById('CpfText') as HTMLInputElement | null;
-    var value = input?.value;
-
-    if (value == '' || value == null) {
-      return;
-    }
-
-    let valorFormatado = value + '';
-
-    value = this.desformatAnyCpf(valorFormatado);
-
-    var i = 0;
-    while (value[i]) {
-      if (value[i] != '0') {
-        break;
-      } else {
-        i++;
-      }
-    }
-    value = value.substring(i);
-    valorFormatado = value;
-
-    this.dataForm.cpf = valorFormatado;
-    return valorFormatado;
-  }
-
-  public desformatAnyCpf(strCPF: string): string {
+  public undoFormatAnyCpf(strCPF: string): string {
     do {
       if (strCPF.includes('.') || strCPF.includes('-')) {
         strCPF = strCPF.replace('.', '');
@@ -225,7 +170,7 @@ export class AppComponent implements OnInit {
   }
 
   public TestaCPF(): boolean {
-    var strCPF = this.desformatAnyCpf(this.dataForm.cpf);
+    var strCPF = this.undoFormatAnyCpf(this.dataForm.cpf);
     var Soma;
     var Resto;
     Soma = 0;
@@ -248,66 +193,10 @@ export class AppComponent implements OnInit {
     return true;
   }
 
-  public formatName() {
-    const me = document.getElementById('NameText') as HTMLInputElement | null;
-
-    if (me == null) {
-      return;
-    }
-
-    if (me.value.startsWith(' ')) {
-      me.value = me.value.substring(1);
-    }
-
-    if (me.value.endsWith('  ')) {
-      me.value = me.value.substring(0, me.value.length - 1);
-    }
-  }
-
-  public nameVerify(): boolean {
-    const me = document.getElementById('NameText') as HTMLInputElement | null;
-
-    if (me?.value.trim() == '' && me?.value.length == 0) {
-      return false;
-    }
-
-    if (me?.value.trim() == '' && me?.value.length > 0) {
-      alert('Existe apenas espaços no nome!!!');
-      return false;
-    }
-
-    if (me?.value.endsWith(' ')) {
-      me.value = me.value.substring(0, me.value.length - 1);
-    }
-
-    if (me == null) {
-      return false;
-    }
-
-    if (!this.onlyLetters(me.value)) {
-      alert('Existe caracteres especiais no nome!!!');
-    } else {
-      return true;
-    }
-
-    return false;
-  }
-
-  public onlyLetters(str: string): boolean {
-    str = str.trim().toLowerCase();
-    return /^[a-zA-Z\s]+$/.test(str);
-  }
-
-  public onlyNumbers(str: string): boolean {
-    return /^[0-9]+$/.test(str);
-  }
-
-  //------------------------------------------------------------------------------------------
-
   public setMaskCPF() {
     var cpf = this.dataForm.cpf;
-    var num = cpf.replace(/[^\d]/g, ''); //remove todos os caracteres não numéricos
-    var len = num.length; //guarda o número de digitos até o momento
+    var num = cpf.replace(/[^\d]/g, ''); //remove all characters non numerics
+    var len = num.length; //save the length of the num
 
     if (len <= 6) {
       cpf = num.replace(/(\d{3})(\d{1,3})/g, '$1.$2');
@@ -322,7 +211,7 @@ export class AppComponent implements OnInit {
 
   public setMaskCEP() {
     var cep = this.dataForm.cep;
-    var num = cep.replace(/[^\d]/g, ''); //remove todos os caracteres não numéricos
+    var num = cep.replace(/[^\d]/g, ''); //remove all characters non numerics
     cep = num.replace(/(\d{5})(\d{1,3})/g, '$1-$2');
     if (cep.length == 10) cep = cep.substring(0, cep.length - 1);
     this.dataForm.cep = cep;
@@ -337,20 +226,42 @@ export class AppComponent implements OnInit {
       .substring(6, 8)
       .replace(/\s+/g, '')
       .replace(/[^a-z0-9]/gi, '');
+
     // 123456/SP
     crm = numPart.replace(/ ([0-9]{1,6}) /g, '$1');
     if (lettersPart.toUpperCase() != '') {
-      crm += `/` + lettersPart.toUpperCase().replace(/ ([A-Z]{0,2}) /g, '$1');
+      crm += `/` + lettersPart.toUpperCase().replace(/[0-9]/g, '');
     }
     this.dataForm.crm = crm;
   }
 
-  public setMaskOnlyLetters(id: string) {
-    console.log('id: ' + id);
-    var input = document.getElementById(id) as HTMLInputElement;
-    input.value = input.value.replace(
+  public setMaskOnlyLetters(input: string) {
+    input = this.removeSpacesInInitial(input);
+
+    // input.value = this.uppercaseFirstLetter(input.value);
+    input = input.replace(
       /[^a-zA-ZâãàáéèêẽîĩìíôõóòũûúùÂÃÀÁÉÈÊẼÎĨÌÍÔÕÓÒŨÛÚÙ ]/gi,
       ''
     );
+  }
+
+  public setMaskNumbersLettersHyphen(str: string) {
+    var input = document.getElementById(str) as HTMLInputElement;
+    // input.value = this.uppercaseFirstLetter(input.value);
+    input.value = input.value.replace(
+      /[^a-zA-ZâãàáéèêẽîĩìíôõóòũûúùÂÃÀÁÉÈÊẼÎĨÌÍÔÕÓÒŨÛÚÙ 0-9]/gi,
+      ''
+    );
+  }
+
+  public removeSpacesInInitial(str: string): string {
+    do {
+      if (str.charAt(0) === ' ') {
+        str = str.substring(1);
+      } else {
+        break;
+      }
+    } while (true);
+    return str;
   }
 }
